de922242a379022f1d639728765895fe2cc778e2
Mike Fallesen <mfallesen2010@gmail.com>
2021-06-17 14:38:54 -0700

    add empty placeholder file for model, add Empty test for model

--- tests/server.test.js
+++ tests/server.test.js
@@ -3,12 +3,12 @@ const app = express();
 
 describe('Test Server Connection', ()=> {
 
-    let port = 8787
-
-    app.listen(port, () => console.log(`port open on ${port}`))
-
-
-    it('Should be port 8787', () => {
+    
+    
+    
+    it('Should be port 8787', async () => {
+        let port = 8787
+        app.listen(port,  () => console.log(`port open on ${port}`))
         expect(port).toBe(8787)
     })
 })
