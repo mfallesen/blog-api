9e417bd49862d4c07b9ca01f1347624a0731ba97
Mike Fallesen <mfallesen2010@gmail.com>
2021-06-17 14:37:45 -0700

    add db connection to test Mongo database

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express();
-const port = 6000;
+const port = 6060;
+const mongoose = require('mongoose')
+const db = mongoose.connection;
+
+mongoose.connect('mongodb://localhost:27017/blogapi-test', 
+{ 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log("Connected to Database")
+})
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 });
 
 app.listen(port, () => {
-  console.log(`Port Open on ${port}!`)
+    console.log(`Port Open on ${port}!`)
 });
